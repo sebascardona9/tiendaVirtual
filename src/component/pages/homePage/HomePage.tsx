83071af603f0a8d29a9b9886e722b821e63e4879
Sebastian Cardona <sebascardona9@gmail.com>
2026-03-15 18:52:47 +0100

    feat: eliminar sección de reseñas hardcodeadas de la homepage

--- src/component/pages/homePage/HomePage.tsx
+++ src/component/pages/homePage/HomePage.tsx
@@ -2,7 +2,6 @@ import HeroSection       from "./sections/HeroSection"
 import CategoryCards     from "./sections/CategoryCards"
 import ProductsSection   from "./sections/ProductsSection"
 import CandleTypesSection from "./sections/CandleTypesSection"
-import ReviewsSection    from "./sections/ReviewsSection"
 
 const HomePage = () => (
     <div className="w-full">
@@ -10,7 +9,6 @@ const HomePage = () => (
         <CategoryCards />
         <ProductsSection />
         <CandleTypesSection />
-        <ReviewsSection />
     </div>
 )
 
